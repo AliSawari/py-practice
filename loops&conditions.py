747d3e20973d6e4de539f8c8253951c7506603df
hossein <ali.dragon.swr@gmail.com>
2018-11-13 17:28:08 +0330

    adding operators

---
+++ loops&conditions.py
@@ -0,0 +1,15 @@
+# conditions, Loops, Logical operators
+
+# operators
+# equals/not to 
+print(3 == 2) # outputs False
+print(4 == 4) # outputs True
+print(4 != 4) # outputs False
+
+# greater/less/and equal then
+print(5 > 6) # outputs False
+print(2 < 2) # outputs False
+print(2 <= 2) # outputs True
+print(5 >= 4) # outputs True
+
+
