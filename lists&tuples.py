c15998c580101df967cf76c4b6995b37d159c96c
hossein <ali.dragon.swr@gmail.com>
2018-11-14 17:15:14 +0330

    init lists :smiley:

---
+++ lists&tuples.py
@@ -0,0 +1,9 @@
+# lists 
+# lists are same as arrays, they can hold multiple and different values
+
+fruits = ['banana','apple','orange','grape']
+
+print(fruits);
+
+# add an item to the list
+fruits.append('pineapple')
