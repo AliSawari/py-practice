a23830da78284cef4b44eae90c068e3b14a089c2
hossein <ali.dragon.swr@gmail.com>
2018-11-14 20:08:26 +0330

    adding types.py

---
+++ types.py
@@ -0,0 +1,35 @@
+# this is the python data types :
+
+# first of all print() is a function which will print the result to the output
+print("Hi Welcome!")
+
+# strings
+myName = 'Ali Sawari'
+
+# whole numbers
+myNum = 3.14
+
+# floating numbers
+myFlo = 14.6
+
+# booleans
+myBool = True
+
+# lists(arrays)
+myList = [1,2,'Hello',3.14]
+
+# tuples (constant arrays)
+myTuple = (1,2,3,4)
+
+# functions
+def MyFunc():
+  print('Hello')
+
+# classes
+class MyClass:
+  print('Hello')
+
+# objects
+myObj = object()
+# or creating an object from a class 
+myObj = MyClass()
