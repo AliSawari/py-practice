f681206c6ac68c477dec838bcc951872dd2ed723
hossein <ali.dragon.swr@gmail.com>
2018-11-16 19:43:29 +0330

    more on functions

--- functions.py
+++ functions.py
@@ -4,3 +4,17 @@
 def add(a , b):
   c = a + b
   return c
+
+# calling a function and storing the result in a variable
+result = add(3, 5)
+print(result)
+
+# alternatively you can print out the result directly
+print(add(12,90))
+
+# a simple function that returns your info
+def sayHi(name, age):
+  return "Hello {} you are {} years old ".format(name, age)
+
+
+print(sayHi("Ali",20))
