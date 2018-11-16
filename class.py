aaf0c47bc18045f605cf40f4cf805eb9d4c0798e
hossein <ali.dragon.swr@gmail.com>
2018-11-16 19:42:53 +0330

    more on classes

--- class.py
+++ class.py
@@ -8,6 +8,9 @@ class Person:
     # assigning properties to the object based on the input data
     self.name = name
     self.age = age
+    
+  # some variables in the class that will be accessible in the object
+  classRule = 'creating some Persons'
 
   # defining methods to our new object
   # we need self to get our properties
@@ -20,11 +23,13 @@ Ali = Person('Ali Sawari', 20)
 # getting properties
 print(Ali.name)
 print(Ali.age)
+print(Ali.classRule)
 
 # calling methods
 print(Ali.sayHi())
 
 Ahmad = Person('Ahmad', 19)
 print(Ahmad.name)
+print(Ahmad.classRule)
 print(Ahmad.age)
 print(Ahmad.sayHi())
