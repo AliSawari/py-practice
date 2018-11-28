b41d32a1d311915c0c7ddbd22a5242e7bfd93478
hossein <ali.dragon.swr@gmail.com>
2018-11-28 17:17:53 +0330

    adding functions

--- functions.py
+++ functions.py
@@ -13,14 +13,40 @@ print(result)
 print(add(12,90))
 
 # a simple function that returns your info
+# using the format() method in string
 def sayHi(name, age):
-  return "Hello {} you are {} years old ".format(name, age)
-
+  return "Hi, my name is {} and I am {} years old.".format(name, age)
 
 print(sayHi("Ali",20))
 
 
+
+
 # Python Built-in functions
+
+
+# the range() function gives us a range of data from a start to an end
+# lets print from 0 to 5
+for x in range(0,5):
+  print(x)
+# it prints only to 4, because 0 is included, so we practically are printing 5 values
+
+# the pow() function takes two numbers to execute x in power of y
+nu = 2
+print(pow(nu, 3))
+
+
+# lets write the above function from scratch :)
+# notice we already have a function for that in python which is pow()
+# but lets just see how its done
+def power(a,b):
+  r = 1
+  for x in range(0, b):
+    r *= a
+  return r
+
+print(power(2,4))
+
 
 # the len function returns the length of a list or string
 here = 'something'
@@ -29,15 +55,39 @@ print("Length of here: ", len(here))
 # convertion 
 # use these functions to convert types 
 num = 4
+myL = ['one', 'two', 'three']
 # to integer 
 print(int(num))
 # to float
 print(float(num))
 # to string
 print(str(num))
+# to tuple
+tu = tuple(myL)
+print(tu)
+# to list
+l2 = list(tu)
+print(l2)
 
-# hash returns the hash value to a value
+
+# hash returns the hash value of a value
 print(hash('HashThis'))
 
 # hex returns the lowercase hex value prefixed with 0x from an integer
 print(hex(651))
+
+# the open() function will take the path for a file and opens it
+# second argument to open() is the mode, r for reading, w writing and e executing
+# then we can read the data in file.txt
+# I will open file.txt
+txt = open('file.txt', 'r').read()
+print(txt)
+
+# alternatively, we can seperate the data by each line, we will use split() function
+# which is a method for any string
+lines = txt.split('\n')
+print(lines)
+
+# lets append line1 in the beginning of each line
+for x in lines:
+  print("Line", lines.index(x) + 1, x)
